424ed0f466956327b2bf91eb39cf812b727a237c
arpanmangal <mangalarpan@gmail.com>
2019-03-16 10:07:41 +0530

    QR works

--- lab2_omp.cpp
+++ lab2_omp.cpp
@@ -8,7 +8,7 @@ int min (int a, int b) {
     return (a < b) ? a : b;
 }
 
-void MatrixMultiply (double *A, double* B, double* C, int m, int n, int p, int parallelise=1)
+void MatrixMultiply (float *A, float* B, float* C, int m, int n, int p, int parallelise=1)
 {
     // C = A * B
     // dim (A) = m * n
@@ -66,15 +66,15 @@ void MatrixMultiply (double *A, double* B, double* C, int m, int n, int p, int p
     }
 }
 
-void MatrixSubtract (double *A, double *B, double *C, int m, int n) {
+void MatrixSubtract (float *A, float *B, float *C, int m, int n) {
     int size = m * n;
     #pragma omp parallel for schedule (dynamic, 64)
     for (int i = 0; i < size; i++)
         C[i] = A[i] - B[i];
 }
 
-double VectorNorm (double *A, int m) {
-    double norm = 0;
+float VectorNorm (float *A, int m) {
+    float norm = 0;
     #pragma omp parallel for schedule (dynamic, 64) reduction (+:norm)
     for (int i = 0; i < m; i++)
         norm += A[i] * A[i];
@@ -82,8 +82,8 @@ double VectorNorm (double *A, int m) {
     return sqrt(norm);
 }
 
-double InnerProduct (double *A, double *B, int m) {
-    double IP = 0;
+float InnerProduct (float *A, float *B, int m) {
+    float IP = 0;
     #pragma omp parallel for schedule (dynamic, 64) reduction (+:IP)
     for (int i = 0; i < m; i++)
         IP += A[i] * B[i];
@@ -91,14 +91,14 @@ double InnerProduct (double *A, double *B, int m) {
     return IP;
 }
 
-void ScalarMultiply (double *A, double *B, int m, int n, double alpha) {
+void ScalarMultiply (float *A, float *B, int m, int n, float alpha) {
     int size = m * n;
     #pragma omp parallel for schedule (dynamic, 64)
     for (int i = 0; i < size; i++)
         B[i] = A[i] * alpha;
 }
 
-void makeIdenMatrix (double *I, int m) {
+void makeIdenMatrix (float *I, int m) {
     printf("%d\n", m);
     #pragma omp parallel for schedule (dynamic, 64) collapse (2)
     for (int i = 0; i < m; i++)
@@ -106,28 +106,28 @@ void makeIdenMatrix (double *I, int m) {
             I[i*m + j] = (i == j);
 }
 
-void MatrixTranspose (double *A, double *B, int n) {
+void MatrixTranspose (float *A, float *B, int m, int n) {
     #pragma omp parallel for schedule (dynamic, 64) collapse (2)
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
-            B[i*n + j] = A[j*n + i];
+            B[j*m + i] = A[i*n + j];
 }
 
-void MatrixAssign (double *A, double *B, int m, int n) {
+void MatrixAssign (float *A, float *B, int m, int n) {
     int size = m * n;
     #pragma omp parallel for schedule (dynamic, 64)
     for (int i = 0; i < size; i++)
         B[i] = A[i];
 }
 
-void MatrixProj (double *A, double *U, double *P, int N) {
+void MatrixProj (float *A, float *U, float *P, int N) {
     // All N * 1 Matrix
     // Project A on U
-    double factor = InnerProduct (U, A, N) / InnerProduct (U, U, N);
+    float factor = InnerProduct (U, A, N) / InnerProduct (U, U, N);
     ScalarMultiply (U, P, N, 1, factor);
 }
 
-void makeTriangular (double *M, int N, int upper=1) {
+void makeTriangular (float *M, int N, int upper=1) {
     // Make the matrix triangular
     #pragma omp parallel for schedule (dynamic, 64) collapse (2)
     for (int i = 0; i < N; i++)
@@ -136,7 +136,7 @@ void makeTriangular (double *M, int N, int upper=1) {
                 M[i*N + j] = 0;
 }
 
-void printMatrix (double *M, int m, int n) {
+void printMatrix (float *M, int m, int n) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             printf("%f ", M[i*n + j]);
@@ -146,10 +146,10 @@ void printMatrix (double *M, int m, int n) {
     printf("\n");
 }
 
-void GramSchmidt (double *A, int N, double *At, double *u, double *e, double *p, double *Q, double *R) {
+void GramSchmidt (float *A, int N, float *At, float *u, float *e, float *p, float *Q, float *R) {
     // Just allocate At, u, e matrices to be N*N; p matrix to be N * 1
     
-    MatrixTranspose (A, At, N);
+    MatrixTranspose (A, At, N, N);
 
     for (int k = 0; k < N; k++) {
         MatrixAssign (At + k*N, u + k*N, N, 1);
@@ -158,28 +158,28 @@ void GramSchmidt (double *A, int N, double *At, double *u, double *e, double *p,
             MatrixSubtract (u + k*N, p, u + k*N, N, 1);
         }
 
-        double uNorm = VectorNorm (u + k*N, N);
+        float uNorm = VectorNorm (u + k*N, N);
         ScalarMultiply (u + k*N, e + k*N, N, 1, 1 / uNorm);
     }
 
-    MatrixTranspose (e, Q, N);
+    MatrixTranspose (e, Q, N, N);
     MatrixMultiply (e, A, R, N, N, N);
     // printMatrix(R, N, N);
     makeTriangular (R, N, 1);
 }
 
-void QR (double *D, int N, double *Evals, double *E) {
-    double *lD = (double *) malloc (sizeof(double) * N * N);
+void QR (float *D, int N, float *Evals, float *E) {
+    float *lD = (float *) malloc (sizeof(float) * N * N);
     MatrixAssign (D, lD, N, N);
     makeIdenMatrix (E, N);
 
-    double *At = (double *) malloc (sizeof(double) * N * N);
-    double *u = (double *) malloc (sizeof(double) * N * N);
-    double *e = (double *) malloc (sizeof(double) * N * N);
-    double *p = (double *) malloc (sizeof(double) * N);
-    double *Q = (double *) malloc (sizeof(double) * N * N);
-    double *R = (double *) malloc (sizeof(double) * N * N);
-    double *E_next = (double *) malloc (sizeof(double) * N * N);
+    float *At = (float *) malloc (sizeof(float) * N * N);
+    float *u = (float *) malloc (sizeof(float) * N * N);
+    float *e = (float *) malloc (sizeof(float) * N * N);
+    float *p = (float *) malloc (sizeof(float) * N);
+    float *Q = (float *) malloc (sizeof(float) * N * N);
+    float *R = (float *) malloc (sizeof(float) * N * N);
+    float *E_next = (float *) malloc (sizeof(float) * N * N);
 
     for (int i = 0; i < 10000; i++) {
         // printMatrix (E, N, N);
@@ -207,26 +207,49 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
     omp_set_num_threads(4);
     srand (0); // Deterministically Random
 
-    N = 2;
-    // double a[] = {12, -51, 4, 6, 167, -68, -4, 24, -41};
-    // double a[] = {-2, -4, 2, -2, 1, 2, 4, 2, 5};
-    double a[] = {24, -15, -15, 25};
-    double *A = (double *) malloc (sizeof(double) * N * N);
+    // N = 3, M = 5;
+    // float a[] = {12, -51, 4, 6, 167, -68, -4, 24, -41, 10, 23, 10, 78, 90, 13};
+    // // float a[] = {-2, -4, 2, -2, 1, 2, 4, 2, 5};
+    // // float a[] = {24, -15, -15, 25};
+    // float *A = (float *) malloc (sizeof(float) * M * N);
+    // for (int i = 0; i < M*N; i++)
+    //     A[i] = a[i];
+    // float *B = (float *) malloc (sizeof(float) * N * M);
 
-    for (int i = 0; i < 9; i++)
+    // MatrixTranspose (A, B, M, N);
+    // printMatrix (A, M, N);
+    // printMatrix (B, N, M);
+    // return;
+
+    N = 2;
+    // float a[] = {12, -51, 4, 6, 167, -68, -4, 24, -41};
+    // float a[] = {-2, -4, 2, -2, 1, 2, 4, 2, 5};
+    float a[] = {25, -15, -15, 25};
+    float *A = (float *) malloc (sizeof(float) * N * N);
+
+    for (int i = 0; i < N*N; i++)
         A[i] = a[i];
 
-    double *E_vals = (double *) malloc (sizeof(double) * N);
-    double *E = (double *) malloc (sizeof(double) * N * N);
+    float *E_vals = (float *) malloc (sizeof(float) * N);
+    float *E = (float *) malloc (sizeof(float) * N * N);
 
     QR (A, N, E_vals, E);
 
-    // double *At = (double *) malloc (sizeof(double) * N * N);
-    // double *u = (double *) malloc (sizeof(double) * N * N);
-    // double *e = (double *) malloc (sizeof(double) * N * N);
-    // double *p = (double *) malloc (sizeof(double) * N);
-    // double *Q = (double *) malloc (sizeof(double) * N * N);
-    // double *R = (double *) malloc (sizeof(double) * N * N);
+    return;
+
+    float *Dt = (float *) malloc (sizeof(float) * N * M);
+    MatrixTranspose (D, Dt, M, N);
+
+    float *SVDMatrix = (float *) malloc (sizeof(float) * M * M);
+    MatrixMultiply (D, Dt, SVDMatrix, M, N, M);
+
+
+    // float *At = (float *) malloc (sizeof(float) * N * N);
+    // float *u = (float *) malloc (sizeof(float) * N * N);
+    // float *e = (float *) malloc (sizeof(float) * N * N);
+    // float *p = (float *) malloc (sizeof(float) * N);
+    // float *Q = (float *) malloc (sizeof(float) * N * N);
+    // float *R = (float *) malloc (sizeof(float) * N * N);
 
     // GramSchmidt (A, N, At, u, e, p, Q, R);
 
@@ -249,10 +272,10 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
     // // int m = 2758, n = 4002, p = 2920;
     // int m = 1, n = 10000, p = 1;
     // int I = 5;
-    // double *A = (double *) malloc (sizeof(double) * m * n);
-    // double *B = (double *) malloc (sizeof(double) * n * p);
-    // double *C = (double *) malloc (sizeof(double) * I * I);
-    // double *Cp = (double *) malloc (sizeof(double) * I * I); 
+    // float *A = (float *) malloc (sizeof(float) * m * n);
+    // float *B = (float *) malloc (sizeof(float) * n * p);
+    // float *C = (float *) malloc (sizeof(float) * I * I);
+    // float *Cp = (float *) malloc (sizeof(float) * I * I); 
 
     // for (int a = 0; a < m*n; a++) {
     //     A[a] = (rand() % 100000) / 100000.0;
@@ -266,9 +289,9 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
     //     for (int j = 0; j < I; j++)
     //         C[i*I+j] = rand() % 100;
     
-    // // double start_time = omp_get_wtime();
+    // // float start_time = omp_get_wtime();
     // // MatrixMultiply (A, B, C, m, n, p, 0);
-    // // double end_time = omp_get_wtime();
+    // // float end_time = omp_get_wtime();
     // // printf("Seq time: %f\n", end_time - start_time);
 
     // for (int i = 0; i < I; i++) {
@@ -278,13 +301,13 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
     // }
     //     printf("\n");
 
-    // double start_time = omp_get_wtime();
-    // double norm = VectorNorm (A, n);
+    // float start_time = omp_get_wtime();
+    // float norm = VectorNorm (A, n);
     // // MatrixSubtract (A, A, A, m, n);
     // // ScalarDivide (A, A, m, n, 10);
     // MatrixTranspose(C, Cp, I);
     // // makeIdenMatrix (C, I);
-    // double end_time = omp_get_wtime();
+    // float end_time = omp_get_wtime();
     // for (int i = 0; i < I; i++) {
     //     for (int j = 0; j < I; j++)
     //         printf("%f ", Cp[i*I+j]);
